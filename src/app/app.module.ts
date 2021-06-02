34a23e334e5355db85e50b9823e297c41c5aa155
revolt268 <revolt268@charter.net>
2021-06-02 13:36:04 -0500

    Communicating with Child Component with Inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventListComponent } from './events/events-list.components';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { EventListComponent } from './events/events-list.components';
   ],
   declarations: [
     EventsAppComponent,
-    EventListComponent
+    EventListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/events/events-list.components.ts
+++ src/app/events/events-list.components.ts
@@ -2,11 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  template: `
+  <div>
+    <h1>Upcoming angular Events</h1>
+    <hr/>
+    <event-thumbnail [event]="event1"></event-thumbnail>
+  </div>
+
+  `
 })
 
 export class EventListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect',
     date: '9/26/2036',
