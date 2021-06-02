babe22676aea1ee3ef3ae8e46a15386d165744d2
revolt268 <revolt268@charter.net>
2021-06-02 13:16:30 -0500

    Starting with Angular (Events project)

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+    <h2>Hey Buddy!</h2>
+    <img src="/assets/images/basic-shield.png"/>
+    `
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
---
+++ src/app/events/events-list.components.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'events-list',
+  template: `
+  <div>
+    <h1>Upcoming angular Events</h1>
+  </div>
+  `
+})
+
+export class EventListComponent {
+
+}
