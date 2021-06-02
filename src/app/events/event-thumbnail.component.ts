10b3de1983ab22a6adcfdc43a0f95eda442537c3
revolt268 <revolt268@charter.net>
2021-06-02 14:55:42 -0500

    Using hidden instead of ng

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,20 +4,20 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
   selector: 'event-thumbnail',
   template: `
-  <div class="well hoverwell thumbnail">
-    <h2>{{event?.name}}</h2>
-    <div>Date: {{event?.date}}</div>
-    <div>Time: {{event?.time}}</div>
-    <div>Price: \${{event?.price}}</div>
-    <div *ngIf="event?.location">
-      <span>Location: {{event?.location?.address}}</span>
+    <div class="well hoverwell thumbnail">
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div>Time: {{event?.time}}</div>
+      <div>Price: \${{event?.price}}</div>
+      <div [hidden]="!event?.location">
+        <span>Location: {{event?.location?.address}}</span>
 
-      <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div [hidden]="!event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
+      </div>
     </div>
-    <div *ngIf="event?.onlineUrl">
-      Online URL: {{event?.onlineUrl}}
-    </div>
-  </div>
   `,
   styles:[`
     .thumbnail { min-height: 210px;}
