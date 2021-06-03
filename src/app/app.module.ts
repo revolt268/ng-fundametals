d49601117fb13d253a66ed26fa642b359ef09e99
revolt268 <revolt268@charter.net>
2021-06-03 11:39:48 -0500

    Added Create event and wired up cancel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
 
 import { EventsAppComponent } from './events-app.component';
+import { CreateEventComponent } from './events/create-event-component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
@@ -22,6 +23,7 @@ import { appRoutes } from './routes';
     EventThumbnailComponent,
     NavBarcomponent,
     EventDetailsComponent,
+    CreateEventComponent,
   ],
   providers: [
     EventService,
