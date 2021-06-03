296fa7e43672503ea67a96e2338dc8042798c9df
revolt268 <revolt268@charter.net>
2021-06-03 13:22:47 -0500

    Adding Type Safety with new IEvent interface

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { IEvent } from "./shared";
 
 
 @Component({
@@ -34,7 +35,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 })
 
 export class EventThumbnailComponent {
-  @Input() event:any;
+  @Input() event:IEvent;
 
   getStartTimeClass() {
     // returning objects
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { toBase64String } from "@angular/compiler/src/output/source_map";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ToastrService } from "../common/toastr.service";
+import { IEvent } from "./shared";
 import { EventService } from "./shared/event.service";
 
 @Component({
@@ -19,7 +20,7 @@ import { EventService } from "./shared/event.service";
 })
 
 export class EventsListComponent implements OnInit {
-  events:any;
+  events:IEvent[];
   constructor(private eventService: EventService,
               private toastr: ToastrService,
               private route:ActivatedRoute){
