f1e71612660bfd4c0e924e228e0f00ee9e16a75e
revolt268 <revolt268@charter.net>
2021-06-03 17:10:35 -0500

    Multiple Slot Content with attribute selector

--- src/app/common/collapsible-well.components.ts
+++ src/app/common/collapsible-well.components.ts
@@ -5,9 +5,9 @@ import { Component, Input } from '@angular/core';
   template:`
   <div (click)="toggleContent()" class="well pointable">
     <h4>
-     <ng-content select=".title"></ng-content>
+     <ng-content select="[well-title]"></ng-content>
     </h4>
-    <ng-content *ngIf="visible" select=".body"></ng-content>
+    <ng-content *ngIf="visible" select="[well-body]"></ng-content>
   </div>
   `
 })
