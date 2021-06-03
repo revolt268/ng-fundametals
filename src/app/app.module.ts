ccb6c1c321410d622603d89d579784ae79f99dda
revolt268 <revolt268@charter.net>
2021-06-03 13:14:06 -0500

    Barreling imports through index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
 import { Error404Component } from './errors/404.component';
-
+import {
+  EventsListComponent,
+  EventService,
+  EventListResolver,
+  EventThumbnailComponent,
+  EventRouteActivator,
+  CreateEventComponent,
+  EventDetailsComponent
+} from './events/index'
 import { EventsAppComponent } from './events-app.component';
-import { CreateEventComponent } from './events/create-event-component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventListResolver } from './events/events-list-resolver.service';
-import { EventsListComponent } from './events/events-list.component';
-import { EventService } from './events/shared/event.service';
 import { NavBarcomponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
 
