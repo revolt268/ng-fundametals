828c94e49e95aac69803c07dae96b8ac28eea328
revolt268 <revolt268@charter.net>
2021-06-04 09:11:21 -0500

    Pipe date examples

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { IEvent } from "./shared";
   template: `
     <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
       <h2>{{event?.name | uppercase}}</h2>
-      <div>Date: {{event?.date}}</div>
+      <div>Date: {{event?.date | date:'shortDate'}}</div>
       <div [ngStyle]="getStartTimeStyle()"
             [ngSwitch]="event?.time">
         Time: {{event?.time}}
