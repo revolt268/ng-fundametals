b82dd1003853925c22a789c037e9e37106c58c36
revolt268 <revolt268@charter.net>
2021-06-04 09:09:59 -0500

    Simple pipe upper case examples

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { IEvent } from "./shared";
   selector: 'event-thumbnail',
   template: `
     <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-      <h2>{{event?.name}}</h2>
+      <h2>{{event?.name | uppercase}}</h2>
       <div>Date: {{event?.date}}</div>
       <div [ngStyle]="getStartTimeStyle()"
             [ngSwitch]="event?.time">
