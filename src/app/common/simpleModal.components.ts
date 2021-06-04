b56b3b61842015c6f205f731903aae5663271b54
revolt268 <revolt268@charter.net>
2021-06-04 16:19:59 -0500

    Adding an option to handle if the modal is closed

--- src/app/common/simpleModal.components.ts
+++ src/app/common/simpleModal.components.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from "@angular/core";
-
+import { Component, ElementRef, Inject, Input, ViewChild } from "@angular/core";
+import { JQ_TOKEN } from "./jQuery.service";
 
 @Component({
   selector: 'simple-modal',
   template:`
-  <div id="{{elementId}}" class="modal fade" tabindex="-1">
+  <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -13,7 +13,7 @@ import { Component, Input } from "@angular/core";
           </button>
           <h4 class="modal-title">{{title}}</h4>
         </div>
-        <div class="modal.body">
+        <div class="modal-body" (click)="closeModal()">
           <ng-content></ng-content>
         </div>
       </div>
@@ -28,4 +28,14 @@ import { Component, Input } from "@angular/core";
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @Input() closeOnBodyClick: string;
+  @ViewChild('modalcontainer') containerEl : ElementRef;
+
+  constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+  closeModal(){
+    if (this.closeOnBodyClick.toLocaleLowerCase() === "true"){
+       this.$(this.containerEl.nativeElement).modal('hide');
+      }
+  }
 }
