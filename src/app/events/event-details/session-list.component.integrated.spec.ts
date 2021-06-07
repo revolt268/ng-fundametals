0517fac7fe682b3d27a580dad41913f056fea563
revolt268 <revolt268@charter.net>
2021-06-07 10:57:30 -0500

    Test setup, it isn't working so we need to fix it

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -1,6 +1,7 @@
 import { DebugElement } from "@angular/core"
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { AuthService } from "src/app/user/auth.service";
+import { DurationPipe } from "../shared";
 import { SessionListComponent } from "./session-list.component"
 import { VoterService } from "./voter.service";
 
@@ -17,7 +18,8 @@ describe("SessionListComponent", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations:[
-        SessionListComponent
+        SessionListComponent,
+        DurationPipe
       ],
       providers: [
         { provide: AuthService, useValue: mockAuthService },
@@ -31,6 +33,18 @@ describe("SessionListComponent", () => {
   })
 
   describe('initial display', () => {
+    it('should have the correct title', () => {
+      // Aranged component
+      component.sessions = [
+        {name: 'Session 1', id: 3, presenter: 'Joe', duration: 1, level: 'beginner', abstract: 'abstract', voters: ['john', 'bob']}
+      ]
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+      component.ngOnChanges();
 
+      // the Action
+      fixture.detectChanges();
+    })
   })
 })
