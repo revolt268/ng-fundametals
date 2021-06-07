0f46b85d89355ffc9292b1f13efe7c71fcad62d0
revolt268 <revolt268@charter.net>
2021-06-07 08:38:16 -0500

    Updated voter service to HTTP

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -13,6 +13,7 @@ export class SessionListComponent implements OnChanges
   @Input() sessions: ISession[];
   @Input() filterBy: string;
   @Input() sortBy: string;
+  @Input() eventId: number;
   visibleSessions: ISession[] = [];
 
   constructor(private auth: AuthService,
@@ -37,9 +38,9 @@ export class SessionListComponent implements OnChanges
 
   toggleVote(session: ISession){
     if(this.userHasVoted(session)){
-      this.voterService.deleteVoter(session, this.auth.currentUser.userName);
+      this.voterService.deleteVoter(this.eventId, session, this.auth.currentUser.userName);
     } else {
-      this.voterService.addVoter(session, this.auth.currentUser.userName);
+      this.voterService.addVoter(this.eventId, session, this.auth.currentUser.userName);
     }
     if(this.sortBy === 'votes'){
       this.visibleSessions.sort(sortByVotesDesc);
@@ -59,7 +60,7 @@ const sortByNameAsc = (s1: ISession, s2: ISession) =>{
   else return -1
 }
 
-// This function is techincally scoped globally and can be overriden 
+// This function is techincally scoped globally and can be overriden
 function sortByVotesDesc(s1: ISession, s2: ISession) {
   return s2.voters.length - s1.voters.length;
 }
