911a020112960e1f9c1976fb1208f1627fc75aeb
revolt268 <revolt268@charter.net>
2021-06-07 09:03:03 -0500

    Added Persisting auth

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -26,12 +26,33 @@ export class AuthService {
       .pipe(catchError(err => {
         return of(false)
       }))
-
-
   }
 
   isAuthenticated(){
     return !!this.currentUser;
+  }
+
+  checkAuthenticationStatus() {
+    // This is one way we can do this, but we can also use the tap call as well
+    //  call back is within the subscribe
+    /* this.http.get('/api/currentIdentity')
+      .subscribe(data => {
+        if(data instanceof Object) {
+          this.currentUser = <IUser>data;
+        }
+      }); */
+
+      // callback is with the tap method instead of the subscribe
+      //  The benefit of this is that we can use this code if we want
+      //  components to react to the change by changing this to a return
+      //  and adding the subscribe on their end
+      this.http.get('/api/currentIdentity')
+        .pipe(tap(data => {
+          if(data instanceof Object) {
+            this.currentUser = <IUser>data;
+          }
+        }))
+        .subscribe();
   }
 
   updateCurrentUser(firstName: string, lastName:string){
